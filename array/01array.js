020e902cedb762e60cc78ab3f8b3128e36d3ee40
GST-Slava <gstslava@gmail.com>
2022-04-05 20:04:49 +0300

    first commit fcc

---
+++ array/01array.js
@@ -0,0 +1,6 @@
+let yourArray = ['first', 10, 'second', true, undefined, null, false, 0];
+console.log(yourArray.length);//simple array-простой массив
+
+let myArray = [0, 1, 1, 2, 3, 5, 8, 13];
+console.log(myArray.length);//simple array length 8--массив с длинной в 8 индексов(начинаются с 0)
+
