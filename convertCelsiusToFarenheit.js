1de2b1f17888983fb2fb59b7904103ce0bb6b835
GST-Slava <gstslava@gmail.com>
2022-04-07 22:42:55 +0300

    add 2 files js fcc

---
+++ convertCelsiusToFarenheit.js
@@ -0,0 +1,10 @@
+function convertToF(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    return fahrenheit;
+}
+
+convertToF(-30);
+convertToF(-10);
+convertToF(0);
+convertToF(20);
+convertToF(30);
